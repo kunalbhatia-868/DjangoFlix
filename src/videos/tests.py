b64b069822c61314fa2652e21096a7c45583a0c6
kunal <kunalbhatia868@gmail.com>
2021-11-19 13:39:23 +0530

    tests added for Video model

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Video
+from django.utils import timezone
 
 # Create your tests here.
 
@@ -7,6 +8,8 @@ from .models import Video
 class VideoModelTestCase(TestCase):
     def setUp(self):
         Video.objects.create(title="This is my Title")
+        Video.objects.create(title="This is my Title",state=Video.VideoStateOptions.PUBLISH)
+
 
     def test_valid_title(self):
         title = "This is my Title"
@@ -16,4 +19,17 @@ class VideoModelTestCase(TestCase):
     def test_created_count(self):
         title = "This is my Title"
         qs = Video.objects.all()
-        self.assertEqual(qs.count(), 1)
+        self.assertEqual(qs.count(), 2)
+
+    def test_draft_case(self):
+        qs=Video.objects.filter(state=Video.VideoStateOptions.DRAFT)
+        self.assertEqual(qs.count(),1)
+
+    def test_publish_case(self):
+        qs=Video.objects.filter(state=Video.VideoStateOptions.PUBLISH)
+        published_qs=Video.objects.filter(
+            state=Video.VideoStateOptions.PUBLISH,
+            publish_timestamp__lte=timezone.now()
+        )
+        self.assertEqual(qs.count(),1)
+        self.assertTrue(published_qs.exists())
